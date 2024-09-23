d490fd6f8618be17cbddefffe61442312af4a4a5
Jbernardiss <joaobernardiss@gmail.com>
2024-09-23 02:22:34 -0300

    removed testing line on script

--- src/routes/client.route.ts
+++ src/routes/client.route.ts
@@ -13,7 +13,6 @@ router.use(express.static(path.join(__dirname, '../../client/dist')));
 
 router.get(['', '/despesas', '/ativos', '/perfil'], (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html') );
-  console.log(req.cookies.userId);
 })
 
 export default router;
